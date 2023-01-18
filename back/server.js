2159db996722637c75a6428dc01f8788b42caebf
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2023-01-17 20:55:31 -0500

    Probando servidor

--- back/server.js
+++ back/server.js
@@ -1,2 +1,12 @@
-const app = require('app');
+const app = require('./app');
 
+//Setear el archivo de configuracion
+
+const dotenv = require('dotenv');
+dotenv.config({path: 'back/config/config.env'}) //archico de configuracion principal
+
+const server= app.listen(process.env.PORT, () => {
+
+  console.log("listening on port: " + process.env.PORT + " en modo: " + process.env.NODE_ENV);  
+
+});
