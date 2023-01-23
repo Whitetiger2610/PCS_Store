bf8cddd2f57929ee2f8ad1bf8d1a411215eaf6e4
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2023-01-23 18:40:25 -0500

    CRUD completo
    Se agrega Fetch para ver productos

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,16 +1,90 @@
 const producto = require("../models/products")
+const fetch = (url) => import("node-fetch").then(({default:fetch}) => fetch(url));//usurpacion del require
 
 //Ver lista de productos
 
-exports.getProduct=(req,res,next) =>{
+exports.getProduct=async (req,res,next) =>{
+
+    const productos = await producto.find();
 
     res.status(200).json({
         sucess:true,
-        message: "En esta ruta se ven todos los productos"
+        cantidad: productos.length,
+        productos
 
     })
 }
 
+//Ver producto por ID
+
+exports.getProductById = async (req,res,next) => {
+
+    const productos = await producto.findById(req.params.id)
+
+    if (!productos) {
+
+        return res.status(404).json({
+            success: false,
+            message: "No encontramos ese producto"
+        })
+    }
+    res.status(200).json({
+        sucess:true,
+        message: "A continuacion encuentras informacion sobre tu producto",
+        productos
+
+    })
+    
+}
+
+//Update un producto
+
+exports.updateProducto = async(req, res, next) => {
+
+    let product = await producto.findById(req.params.id)
+
+    if (!product) { //Verifico si el productos existe
+
+        return res.status(404).json({
+            success: false,
+            message: "No encontramos ese producto"
+        })
+    }
+    // Si el producto existe, entonces ejecuto actualizacion
+    product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true, //Valido solo los atributos nuevos
+        runValidators:true
+    });
+    //Respondo Ok si el producto se actualiza
+    res.status(200).json({
+
+        success:true,
+        message:"Producto actualizado",
+        product
+    })
+
+}
+
+//Eliminar un producto
+exports.deleteProducto = async(req, res, next) => {
+
+    const product = await producto.findById(req.params.id)
+
+    if (!product) { //Verifico si el productos existe
+
+        return res.status(404).json({
+            success: false,
+            message: "No encontramos ese producto"
+        })
+    }
+
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message:"Producto eliminado correctamente"
+    }) 
+
+}
 //Crear nuevo producto /api/productos
 
 exports.newProduct = async(req,res,next) => {
@@ -21,3 +95,27 @@ exports.newProduct = async(req,res,next) => {
     })
 
 }
+
+//Hablemos de fetch 
+//Fetch es un modulo
+//ver todos los productos
+
+function verProductos(){
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductos();
+
+//ver por id
+
+function verProductoId(id){
+fetch('http://localhost:4000/api/producto/'+ id)
+.then(res=>res.json())
+.then(res=>console.log(res))
+.catch(err=>console.error(err))
+
+}
+//verProductoId("63cb640b136ec895e197daff");
--- back/routes/products.js
+++ back/routes/products.js
@@ -2,11 +2,13 @@ const express=require("express")
 
 const router= express.Router();
 
-const {getProduct, newProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+const {getProduct, newProduct, getProductById, updateProducto, deleteProducto} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 
 router.route("/productos").get(getProduct); // Establecemos desde que ruta queremos ver el getProduct
 router.route("/producto/nuevo").post(newProduct);// Establecemos la ruta para crear producto
-
+router.route("/producto/:id").get(getProductById); // Establecemos la ruta para ver producto by Id
+router.route("/producto/:id").put(updateProducto); //Establecemos ruta de actualizacion
+router.route("/producto/:id").delete(deleteProducto); //Establecemos ruta para eliminar producto
 
 module.exports = router;
 
