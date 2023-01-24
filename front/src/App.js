a72c9d4329af669c149980d53d537032a50bc9a7
Whitetiger2610 <mga.asesor.esolar@gmail.com>
2023-01-23 23:00:02 -0500

    REACT

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda Virtual PCS-STORE</h1>
+      <img src= "./images/logo.png" alt="Logo"></img>
+      
+    </div>
+  );
+}
+
+export default App;
